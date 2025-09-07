70ccb4952c8f204f776142dc99dfe5a1dab62f0a
ayush058s <ayush05singh08@gmail.com>
2025-09-07 17:32:51 +0530

    learning about inheritance

---
+++ inheritance/Main.java
@@ -0,0 +1,29 @@
+package oops.inheritance;
+
+
+
+// every class inherits object class
+public class Main {
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        System.out.println(box1.l + box1.h +  box1.w); // -3
+
+        Box box2 = new Box(6);
+        System.out.println(box2.l + box2.h +  box2.w);// 18
+
+        Box box3 = new Box(box1);
+        System.out.println(box3.l + box3.h +  box3.w); // -3
+
+
+
+
+    }
+
+    /* types
+        single inheritance
+        multilevel inheritance
+        multiple inheritance => does not exist in java => 1 class extends more than 1 classes(interfaces)
+        hierarchical => 1 class is extended by many
+        hybrid => not in java as it is a combination of single and multiple (interfaces)
+    * */
+}
