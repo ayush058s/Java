3fd0cb6c09585f727cb7fae8e403f8a071d021e5
ayush058s <ayush05singh08@gmail.com>
2025-09-14 03:26:05 +0530

    Variables in interfaces are public static final

---
+++ interfaces/CDPlayer.java
@@ -0,0 +1,15 @@
+package oops.interfaces;
+
+import oops.polyMorphism.Media;
+
+public class CDPlayer implements Media {
+    @Override
+    public void start() {
+        System.out.println("Music start");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Music stop");
+    }
+}
---
+++ interfaces/Main.java
@@ -0,0 +1,14 @@
+package oops.interfaces;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        car.brake();
+        car.acc();
+        car.start();
+
+    }
+}
